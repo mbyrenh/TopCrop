715c7906af8eaaeada3fe83336f12758f6355b11
Martin Byrenheid <martin@byrenheid.net>
2015-04-12 09:42:05 +0100

    WIP: Successfully tested addCroft- and addReport-API

--- DataManager.php
+++ DataManager.php
@@ -19,6 +19,19 @@ class DataManager {
         $stmt -> execute ();
     }
 
+    public function farmerExists ($phone) {
+
+        $stmt = $this -> DBConnection -> prepare ("
+            SELECT COUNT(*)
+            FROM farmers
+            WHERE phone = :phone
+        ");
+
+        $stmt -> bindParam ('phone', $phone, PDO::PARAM_STR);
+        $stmt -> execute ();
+        return (intval($stmt->fetchColumn()) == 1);
+    }
+
     public function addCroft ($phone, $croftName, $latitude, $longitude) {
 
         $stmt = $this -> DBConnection -> prepare ("
@@ -29,22 +42,116 @@ class DataManager {
         ");
 
         $stmt -> bindParam ('longitude', $longitude);
-        $stmt -> bindParam ('latitude', $longitude);
+        $stmt -> bindParam ('latitude', $latitude);
         $stmt -> bindParam ('name', $croftName);
         $stmt -> bindParam ('phone', $phone);
 
         $stmt -> execute ();
     }
 
-    public function addReport ($phone, $croftName, $problem, $start, $description, $end = NULL) {
+    public function addReport ($phone, $problem, $start, $description, $latitude, $longitude, $ongoing) {
 
         $stmt = $this -> DBConnection -> prepare ("
-            INSERT INTO reports (croft_id, problem, start, reported, end, description)
-            SELECT c.id, :problem, :start, NOW(), :end, :description 
+            INSERT INTO reports (:phone, problem, start, reported, end, description, latitude, longitude)
+            VALUES (:phone, :problem, :start, NOW(), :end, :description, :latitude, :longitude) 
+        ");
+
+        $stmt -> bindParam ('problem', $problem);
+        $stmt -> bindParam ('start',(new Datetime($start))->format('Y-m-d H:i:s'));
+        if ($ongoing) {
+            $stmt -> bindValue ('end', NULL);
+        } else {
+            $stmt -> bindValue ('end', (new Datetime())->format('Y-m-d H:i:s'));
+        }
+       
+        $stmt -> bindParam ('description', $description);
+        $stmt -> bindParam ('latitude', $latitude);
+        $stmt -> bindParam ('longitude', $longitude);
+        $stmt -> bindParam ('phone', $phone);
+        $stmt -> execute ();
+    }
+
+    public function updatePhone ($oldNumber, $newNumber) {
+
+        $stmt = $this -> DBConnection -> prepare ("
+            UPDATE farmers 
+            SET phone = :newPhone
+            WHERE phone = :phone
+        ");
+
+        $stmt -> bindParam ('phone', $oldNumber);
+        $stmt -> bindParam ('newPhone', $newNumber);
+
+        $stmt -> execute ();
+    } 
+/*
+    public function getReportsForFarmer ($phone) {
+        
+        $stmt = $DBConnection -> prepare ("
+            SELECT *
+            FROM reports
+            WHERE owner     
+        ");
+    }
+
+    public function updateReport ($report_id, $problem, $start, $description, $end = NULL) {
+        
+    }
+
+
+
+    public function getReports ($croftName) {
+
+    }
+
+    public function revokeReport ($)
+*/
+    public function getCrofts ($phone = NULL) {
+
+        if ($phone !== NULL) {
+            $stmt = $this -> DBConnection -> prepare ("
+                SELECT * 
+                FROM crofts c
+                JOIN farmers f ON c.farmer_id = f.id
+                AND f.phone = :phone
+                ");
+
+            $stmt -> bindParam ('phone', $phone);
+        } else {
+            $stmt = $this -> DBConnection -> prepare ("
+                SELECT * 
+                FROM crofts c
+                JOIN farmers f ON c.farmer_id = f.id
+                ");
+        }
+        $stmt -> execute ();
+
+        return $stmt->fetchAll (PDO::FETCH_ASSOC);
+    }
+
+    /*
+    public function getNeighbours ($phone, $croftName, $distance) {
+
+        $stmt = $this -> DBConnection -> prepare ("
+            SELECT f.phone, (((acos( 
+                                sin(
+                                 ((:latitude)*pi()/180)) * sin((c.latitude*pi()/180))+cos((:latitude)*pi()/180))
+                                 * cos((c.latitude*pi()/180)) * cos((((:longitude)-c.longitude)*pi()/180)))
+                                 *180/pi())*60*1.1515) as distance 
             FROM farmers f
             JOIN crofts c ON c.farmer_id = f.id
-            WHERE f.phone = :phone AND c.name = :name
-        ");
+            WHERE distance <= :distance
+            ");
+
+
+
+        /*
+         *  "SELECT *,(((acos(sin((".$latitude."*pi()/180)) * 
+sin((`Latitude`*pi()/180))+cos((".$latitude."*pi()/180)) * 
+cos((`Latitude`*pi()/180)) * cos(((".$longitude."- `Longitude`)*
+pi()/180))))*180/pi())*60*1.1515) as distance FROM `MyTable` 
+WHERE distance <= ".$distance."         *
+
 
         $stmt -> bindParam ('name', $name);
         $stmt -> bindParam ('problem', $problem);
@@ -55,6 +162,7 @@ class DataManager {
 
         $stmt -> execute ();
 
-    }
+
+}*/
 }
 ?>
--- api.php
+++ api.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Continue: - API for changing the phone number
+ *             -> pest reports: Check date of last change against date of report submission
+ *           - listCrofts -> extend by optional number-parameter to restrict results
+ *
+ * Next Step: Implementation and testing of notification
+ */
+
 require_once ('DataManager.php');
 
 if (! isset ($_REQUEST['operation'])) {
@@ -9,12 +17,121 @@ if (! isset ($_REQUEST['operation'])) {
 switch ($_REQUEST['operation']) {
 
     case 'register':
-        $phone = $_REQUEST['phone'];
-        $DataManager = new DataManager ();
-        $DataManager -> addFarmer ($phone);
-        echo "Done!";
+        if (! isset ($_REQUEST['phone']) || strlen ($_REQUEST['phone']) == 0) {
+            $result = [
+                'result' => -1,
+                'msg'    => 'No phone number given!'
+            ];
+        } else {
+            $phone = $_REQUEST['phone'];
+            $DataManager = new DataManager ();
+            $DataManager -> addFarmer ($phone);
+            $result = [
+                'result' => 1
+            ];
+        } 
     break;
 
+    case 'changeNumber':
+
+        $oldNumber = $_REQUEST['oldNumber'];
+        $newNumber = $_REQUEST['number'];
+        $DataManager = new DataManager ();
+        $DataManager -> updatePhone ($oldNumber, $newNumber);
+        
+        $result = [
+                'result' => 0
+        ];
+    break;
+
+    case 'listCrofts':
+        $DataManager = new DataManager ();
+        if (isset ($_REQUEST['number']) && is_numeric($_REQUEST['number'])) {
+            $crofts = $DataManager -> getCrofts ($_REQUEST['number']);
+        } else {
+            $crofts = $DataManager -> getCrofts ();
+        }
+        $result = [
+            'result' => 0,
+            'data'   => $crofts
+        ];
+
+    break;
+
+    case 'addReport':
+
+        $phone = $_REQUEST['number'];
+        $problem = $_REQUEST['condition'];
+        $start = $_REQUEST['startDate'];
+        $ongoing = $_REQUEST['onGoing'];
+        $description = $_REQUEST['description'];
+        $latitude = $_REQUEST['lat'];
+        $longitude = $_REQUEST['lng'];
+
+        $DataManager = new DataManager ();
+        $DataManager -> addReport (
+            $phone,
+            $problem,
+            $start,
+            $description,
+            $latitude,
+            $longitude,
+            $ongoing);
+
+        $result = [
+            'result' => 0,
+        ];
+    break;
+
+    case 'addCroft': 
+        if (! isset ($_REQUEST['number']) || strlen ($_REQUEST['number']) == 0) {
+            $result = [
+                'result' => -1,
+                'msg'    => 'No phone number given!'
+            ];
+        } 
+        elseif (! isset ($_REQUEST['name']) || strlen ($_REQUEST['name']) == 0) {
+            $result = [
+                'result' => -1,
+                'msg'    => 'No croft name given!'
+            ];
+        }
+        elseif (! isset ($_REQUEST['lat']) || is_numeric ($_REQUEST['lat']) == FALSE) {
+            $result = [
+                'result' => -1,
+                'msg'    => 'No or invalid latitude given!'
+            ];
+        }       if (! isset ($_REQUEST['lng']) || is_numeric ($_REQUEST['lng']) == FALSE) {
+            $result = [
+                'result' => -1,
+                'msg'    => 'No or invalud longitude given!'
+            ];
+        } else {
+            $phone = $_REQUEST['number'];
+            $croftName = $_REQUEST['name'];
+            $latitude = $_REQUEST['lat'];
+            $longitude = $_REQUEST['lng'];
+
+            $DataManager = new DataManager ();
+
+            if (! $DataManager->farmerExists ($phone)) {
+                $DataManager -> addFarmer ($phone);
+            }
+        
+            $DataManager -> addCroft ($phone, $croftName, $latitude, $longitude);
+            $result = [
+                'result' => 0
+            ];
+        }
+        break;
+
+    default:
+        $result =  [
+            'result' => -1,
+            'msg'    => 'Unknown operation given!'
+        ];
 }
+
+echo json_encode ($result);
 
 ?>
